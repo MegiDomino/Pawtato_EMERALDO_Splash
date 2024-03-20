a90d844c9a2ecde47cd6af3b2cf479f14db1deb0
blodv <blodv@LEROI>
2024-03-20 23:48:56 +0800

    Add project files.

---
+++ PawtatoSplash/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PawtatoSplash.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
